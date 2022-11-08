d6568d06683367ac7e2e4df80a9e8553c8a8f391
Lazar Bućan <81108287+LazarB7@users.noreply.github.com>
2022-11-09 00:55:39 +0100

    Update MockERC20.sol

--- test/mocks/MockERC20.sol
+++ test/mocks/MockERC20.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-//contract MVP is ERC20("MVPWAIR", "MVPW", 18) {
-   // constructor(address user) {
-        //_mint(user, 100000e18);
-   //}
-//}
+contract MVP is ERC20("MVPWAIR", "MVPW", 18) {
+    constructor(address user) {
+        _mint(user, 100000e18);
+   }
+}
